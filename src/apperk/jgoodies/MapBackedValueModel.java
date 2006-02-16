d5c5f260d78593a6ea86b94c532de822d5112633
jbalint <jbalint@29d542d7-f3b5-4f65-818e-aafea093d2fb>
2006-02-16 00:30:37 +0000

    Checking in old MapBackedValueModel
    git-svn-id: file:///mnt/Kdrive/important/My_Documents/Code_Live/svn/apperk/trunk@8 29d542d7-f3b5-4f65-818e-aafea093d2fb

---
+++ src/apperk/jgoodies/MapBackedValueModel.java
@@ -0,0 +1,49 @@
+package apperk.jgoodies;
+
+import java.util.Map;
+
+import com.jgoodies.binding.value.AbstractValueModel;
+
+/**
+ * A ValueModel implementation that uses a map entry
+ * instead of a JavaBean property. This version does
+ * NOT provide property change notification.
+ */
+public class MapBackedValueModel extends AbstractValueModel
+{
+	protected Map map;
+	protected String key;
+
+	/**
+	 * Create a new instance.
+	 *
+	 * @param map The map to use to access the entry.
+	 * @param key The key loopup the entry.
+	 */
+	public MapBackedValueModel(Map map, String key)
+	{
+		this.map = map;
+		this.key = key;
+	}
+
+	/**
+	 * Retrieve the value.
+	 *
+	 * @return the value.
+	 */
+	public Object getValue()
+	{
+		return map.get(key);
+	}
+
+	/**
+	 * Set the value.
+	 *
+	 * @param newValue the new value.
+	 */
+	public void setValue(Object newValue)
+	{
+		map.put(key, newValue);
+	}
+}
+
